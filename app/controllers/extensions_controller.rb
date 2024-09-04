7da69116831512fec659ac52dc1cb471bec422bc
Sebi <sebastianmacovei3@gmail.com>
2024-09-05 00:29:52 +0300

    remove empty files for zeitwerk check

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -1,27 +1,27 @@
 class ExtensionsController < ApplicationController
   def index
-    extension = Expense.all
+    extension = Extension.all
     render json: extension
   end
 
   def find_by_id
-    extension = Expense.find(params[:id])
+    extension = Extension.find(params[:id])
     render json: extension
   end
 
   def create
-    extension = Expense.create(extension_params)
+    extension = Extension.create(extension_params)
     render json: extension
   end
 
   def update
-    extension = Expense.find(params[:id])
+    extension = Extension.find(params[:id])
     extension.update(extension_params)
     render json: extension
   end
 
   def destroy
-    extension = Expense.find(params[:id])
+    extension = Extension.find(params[:id])
     extension.destroy
     render json: extension
   end
